a4faa5ac45486939fbba82fd0abb506e03a8dcf5
VP THAHIR <vpthahir3062003@gmail.com>
2026-06-20 20:34:43 +0530

    Learned clearly the debouncing

--- Week 3/Day3/Day3.js
+++ Week 3/Day3/Day3.js
@@ -1,165 +1,210 @@
-// Shallow Copy
+// // Shallow Copy
+
+// const { resolve, reject } = require("promise");
 
 
 
 
-let user={
-    name:"Ronaldo",
-    Personel:{
-        nation:"Portugal"
-    }
-}
-let copy={...user}
-copy.Personel.nation="Argentina";
-console.log(user.Personel.nation);
+// let user={
+//     name:"Ronaldo",
+//     Personel:{
+//         nation:"Portugal"
+//     }
+// }
+// let copy={...user}
+// copy.Personel.nation="Argentina";
+// console.log(user.Personel.nation);
 
 
 
-// Deep Copy
+// // Deep Copy
 
 
-let userr={
-    name:"Ronaldo",
-    Personel:{
-        nation:"Portugal"
-    }
-}
-let copyy=structuredClone(userr)
-copy.Personel.nation="Argentina";
-console.log(userr.Personel.nation);
+// let userr={
+//     name:"Ronaldo",
+//     Personel:{
+//         nation:"Portugal"
+//     }
+// }
+// let copyy=structuredClone(userr)
+// copy.Personel.nation="Argentina";
+// console.log(userr.Personel.nation);
 
 
-// Weak set()
+// // Weak set()
 
-let a=new Set()
+// let a=new Set()
 
-a.add(10);
-a.add(20);
-a.add(10);
-console.log(a);
-console.log({name:"Thahir"});
-
-
-let b=new WeakSet ();
-b.add({place:"Kodinhi"})
-console.log(b);
+// a.add(10);
+// a.add(20);
+// a.add(10);
+// console.log(a);
+// console.log({name:"Thahir"});
 
 
-let k=[1,2,2,3,4,2,3,3]
-let c=new Set(k);
-console.log(c);
+// let b=new WeakSet ();
+// b.add({place:"Kodinhi"})
+// console.log(b);
 
 
-
-const map = new WeakMap();
-let us={}
-
-
-map.set(us,"Rono");
-console.log(map.get(us));
+// let k=[1,2,2,3,4,2,3,3]
+// let c=new Set(k);
+// console.log(c);
 
 
 
-//  Optional Chaining (?.)
+// const map = new WeakMap();
+// let us={}
+
+
+// map.set(us,"Rono");
+// console.log(map.get(us));
 
 
 
-let e={
-    name:"Ronaldo"
-}
-
-console.log(e.place?.citty);
+// //  Optional Chaining (?.)
 
 
 
-// Nullish coalescing
+// let e={
+//     name:"Ronaldo"
+// }
 
-
-let f=null;
-let f1=f??"thahir";
-console.log(f1);
-
-
+// console.log(e.place?.citty);
 
 
 
-
-let z=[1,2,2,3,3,4,4,5,5,6,6,7,3];
-let y=new Set(z);
-console.log(y);
-let x=[...y];
-console.log(x);
+// // Nullish coalescing
 
 
-// const input = document.getElementById("search");
+// let f=null;
+// let f1=f??"thahir";
+// console.log(f1);
 
-// let timer;
 
-// input.addEventListener("input", () => {
 
-//     console.log("You pressed a key");
 
-//     // Cancel the previous timer
-//     clearTimeout(timer);
 
-//     // Start a new timer
-//     timer = setTimeout(() => {
-//         console.log("Searching...");
-//     }, 1000);
 
-// });
+// let z=[1,2,2,3,3,4,4,5,5,6,6,7,3];
+// let y=new Set(z);
+// console.log(y);
+// let x=[...y];
+// console.log(x);
 
-// let input=document.querySelector("#search");
-// let timerr;
-// input.addEventListener("input",()=>{
-//     console.log("Starting");
-//     clearTimeout(timerr);
-//     setTimeout(()=>{
-//     console.log("compleated");
 
-//     },1000)
+// // const input = document.getElementById("search");
+
+// // let timer;
+
+// // input.addEventListener("input", () => {
+
+// //     console.log("You pressed a key");
+
+// //     // Cancel the previous timer
+// //     clearTimeout(timer);
+
+// //     // Start a new timer
+// //     timer = setTimeout(() => {
+// //         console.log("Searching...");
+// //     }, 1000);
+
+// // });
+
+// // let input=document.querySelector("#search");
+// // let timerr;
+// // input.addEventListener("input",()=>{
+// //     console.log("Starting");
+// //     clearTimeout(timerr);
+// //     setTimeout(()=>{
+// //     console.log("compleated");
+
+// //     },1000)
     
-// })
+// // })
 
 
 
 
-// async function l(){
-// let url=("https://jsonplaceholder.typicode.com/users");
-// try{
-//     let arr=[];
-//     let response=await fetch(url);
-//     let data=await response.json();
+// // async function l(){
+// // let url=("https://jsonplaceholder.typicode.com/users");
+// // try{
+// //     let arr=[];
+// //     let response=await fetch(url);
+// //     let data=await response.json();
 
-//     arr.push(data[0].name.toUpperCase());
-//     arr.push(data[0].company.name);
-//     console.log(data[0].id);
-//   console.log(arr);
+// //     arr.push(data[0].name.toUpperCase());
+// //     arr.push(data[0].company.name);
+// //     console.log(data[0].id);
+// //   console.log(arr);
   
     
     
-// }catch(error){
+// // }catch(error){
+// //     console.log("wrong");
+    
+
+// // }
+// // }
+// // l()
+
+
+// let url=("https://jsonplaceholder.typicode.com/users");
+// let arr=[]
+// fetch(url)
+// .then((response)=>{
+//     return response.json();
+// })
+// .then((data)=>{
+//     arr.push(data[0].name.toUpperCase());
+//     arr.push(data[0].company.name);
+//     console.log(arr);
+    
+// })
+// .catch ((error)=>{
 //     console.log("wrong");
     
+// })
 
-// }
-// }
-// l()
-
-
-let url=("https://jsonplaceholder.typicode.com/users");
-let arr=[]
-fetch(url)
-.then((response)=>{
-    return response.json();
+function sam(a){
+return new Promise((resolve,reject)=>{
+    if(a>6){
+        resolve("yes")
+    }else{
+        reject("no")
+    }
 })
-.then((data)=>{
-    arr.push(data[0].name.toUpperCase());
-    arr.push(data[0].company.name);
-    console.log(arr);
+}
+sam(3).then((res)=>{
+    console.log(res);
+    
+}).catch((err)=>{
+    console.log(err);
     
 })
-.catch ((error)=>{
-    console.log("wrong");
+
+
+let input=document.querySelector("#search");
+
+let timer;
+  
     
-})
+    input.addEventListener("input",()=>{
+  console.log("Starting");
+    clearTimeout(timer);
+        timer=setTimeout(()=>{
+       console.log("Finished");
+       
+    },1000)
+        
+    })
+    
+
+
+
+
+
+
+
+
+
