12675a21e21848e60d42c527b238dae1dec279e1
VP THAHIR <vpthahir3062003@gmail.com>
2026-06-22 21:59:09 +0530

    Day 1

---
+++ Weak5(REACT BASICS)/day1/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name="Thahir"
+  return (
+
+
+    // fragment syntax <> </>
+
+
+    <>
+   <h1 style={{color:"red"}}>HELLO WORLD</h1>
+   <p className='how'>How are you {name}</p>
+   </>
+  );
+}
+
+export default App;
