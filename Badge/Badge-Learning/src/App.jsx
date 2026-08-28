b1adca405e9f388e0a0e1ba22cd7893d80e3de5d
VP THAHIR <vpthahir3062003@gmail.com>
2026-08-28 14:33:22 +0530

    Lazy loading , useCallBack

--- Badge/Badge-Learning/src/App.jsx
+++ Badge/Badge-Learning/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-
+import{Routes,Route,useNavigate} from 'react-router-dom'
+import { lazy,Suspense } from 'react'
 import './App.css'
-import Memo from './Memo'
+const Memo=lazy(()=>import('./Memo'))
 
 function App() {
   const [count, setCount] = useState(0)
+  const navigate=useNavigate()
 
   return (
     <>
-     <Memo/>
+    <h1>Main</h1>
+    <button onClick={()=>navigate('/memo')}>Go To Memo</button>
+<Suspense fallback={<h1>Loading....</h1>}>
+     <Routes>
+         <Route path="/memo" element={ <Memo/>} />
+
+     </Routes>
+     </Suspense>
     </>
   )
 }
