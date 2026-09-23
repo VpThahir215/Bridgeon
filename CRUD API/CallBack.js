4326cd9eb8927453db10851140aa9a913ab578a9
VP THAHIR <vpthahir3062003@gmail.com>
2026-09-23 18:28:40 +0530

    CallBack

---
+++ CRUD API/CallBack.js
@@ -0,0 +1,11 @@
+function main(name,callBack){
+    console.log('Started');
+    
+    console.log("heyyy",name);
+    callBack()
+
+}
+function second(){
+console.log('finished')
+}
+main("Cristianoooooooo",second)
